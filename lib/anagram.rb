3e05575fffdb4c289fafa8f0dc3a5e888a47736f
Roy F Guzman Rodriguez <royguzrod@gmail.com>
2020-02-21 22:21:12 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :word
+  
+  def initialize(word)
+    @word = word
+  end
+  
+  def match(list_of_words)
+    @a = []
+    list_of_words.each do |x|
+      if @word.split("").sort == x.split("").sort
+        @a << x
+      end
+    end
+    @a
+  end
+end 
